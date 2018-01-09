68df4d3f8c19fd032f635977f9ee087fc6e28340
angpao <angpao.kittimasak@gmail.com>
2018-01-09 21:40:40 +0700

    end 7-14

--- public/application.js
+++ public/application.js
@@ -1,21 +1,10 @@
 'use strict';
 
 var mainAppModuleName = 'Main'
-var mainAppModule = angular.module(mainAppModuleName, ['ui.router', 'core','hello']);
+var mainAppModule = angular.module(mainAppModuleName, ['ui.router', 'core','users']);
 
 angular.element(document).ready(function () {
     angular.bootstrap(document.querySelector('#mainApp'), ['Main'], {
         strictDi: true
     });
-});
-
-mainAppModule.controller('NameController', ['$scope', '$http', function ($scope, $http) {
-    $scope.yourName = 'No Name';
-    var users_json = $http.get('/user');
-}]);
-
-mainAppModule.filter('sayhello', function () {
-    return function (name) {
-        return 'Hello, ' + name;
-    };
 });
