048c51bb838bbdaa5ac2e668776d326b0e35a11c
angpao <angpao.kittimasak@gmail.com>
2018-01-09 20:45:48 +0700

    end 7-11

--- public/application.js
+++ public/application.js
@@ -3,6 +3,12 @@
 var mainAppModuleName = 'Main'
 var mainAppModule = angular.module(mainAppModuleName, ['hello']);
 
+angular.element(document).ready(function () {
+    angular.bootstrap(document.querySelector('#mainApp'), ['Main'], {
+        strictDi: true
+    });
+});
+
 mainAppModule.controller('NameController', ['$scope', '$http', function ($scope, $http) {
     $scope.yourName = 'No Name';
     var users_json = $http.get('/user');
