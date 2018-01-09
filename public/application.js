365061f444c66d65cef4786cdacff34b16434c50
angpao <angpao.kittimasak@gmail.com>
2018-01-09 20:52:42 +0700

    end 7-11

--- public/application.js
+++ public/application.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var mainAppModuleName = 'Main'
-var mainAppModule = angular.module(mainAppModuleName, ['hello']);
+var mainAppModule = angular.module(mainAppModuleName, ['ui.router','hello']);
 
 angular.element(document).ready(function () {
     angular.bootstrap(document.querySelector('#mainApp'), ['Main'], {
